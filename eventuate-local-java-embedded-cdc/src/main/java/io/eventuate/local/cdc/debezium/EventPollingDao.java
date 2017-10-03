bf137b3a85c67a9fd14da6fbe0744697ab71fc67
root <overgodofcode@gmail.com>
2017-10-03 09:38:05 +0300

    Making connection lost generic. Removing db specfific connection checks. Making them generic.

--- eventuate-local-java-embedded-cdc/src/main/java/io/eventuate/local/cdc/debezium/EventPollingDao.java
+++ eventuate-local-java-embedded-cdc/src/main/java/io/eventuate/local/cdc/debezium/EventPollingDao.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableMap;
 import org.postgresql.util.PSQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessResourceFailureException;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -44,19 +45,24 @@ public class EventPollingDao {
   }
 
   public List<EventToPublish> findEventsToPublish() {
+    return handleConnectionLost(() -> namedParameterJdbcTemplate.query("SELECT * FROM events WHERE published = 0 ORDER BY event_id ASC limit :limit",
+            ImmutableMap.of("limit", maxEventsPerPolling), new BeanPropertyRowMapper(EventToPublish.class)));
+  }
+
+  public void markEventsAsPublished(List<String> ids) {
+    handleConnectionLost(() -> namedParameterJdbcTemplate.update("UPDATE events SET published = 1 WHERE event_id in (:ids)",
+        ImmutableMap.of("ids", ids)));
+  }
+
+  private <T> T handleConnectionLost(Callable<T> query) {
     int attempt = 0;
 
     while(true) {
       try {
-        return namedParameterJdbcTemplate.query("SELECT * FROM events WHERE published = 0 ORDER BY event_id ASC limit :limit",
-            ImmutableMap.of("limit", maxEventsPerPolling), new BeanPropertyRowMapper(EventToPublish.class));
-      } catch (Exception e) {
+        return query.call();
+      } catch (DataAccessResourceFailureException e) {
 
         logger.error(e.getMessage(), e);
-
-        if (!(e instanceof PSQLException) && !(e.getCause() instanceof PSQLException)) {
-          throw e;
-        }
 
         if (attempt++ >= maxAttemptsForPolling) {
           throw e;
@@ -68,11 +74,9 @@ public class EventPollingDao {
         } catch (InterruptedException | SQLException ie) {
           logger.error(ie.getMessage(), ie);
         }
+      } catch (Exception e) {
+        throw new RuntimeException(e);
       }
     }
-  }
-
-  public void markEventsAsPublished(List<String> ids) {
-    namedParameterJdbcTemplate.update("UPDATE events SET published = 1 WHERE event_id in (:ids)", ImmutableMap.of("ids", ids));
   }
 }

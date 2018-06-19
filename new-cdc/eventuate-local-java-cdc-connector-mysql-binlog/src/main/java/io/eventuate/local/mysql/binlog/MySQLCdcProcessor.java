9074dab2bfe2cb63ede043af2b4fef4b7a5b2853
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-06-19 13:05:44 +0300

    Fixing merge

--- new-cdc/eventuate-local-java-cdc-connector-mysql-binlog/src/main/java/io/eventuate/local/mysql/binlog/MySQLCdcProcessor.java
+++ new-cdc/eventuate-local-java-cdc-connector-mysql-binlog/src/main/java/io/eventuate/local/mysql/binlog/MySQLCdcProcessor.java
@@ -5,8 +5,6 @@ import io.eventuate.local.common.BinlogFileOffset;
 import io.eventuate.local.db.log.common.DbLogBasedCdcProcessor;
 import io.eventuate.local.db.log.common.DbLogClient;
 import io.eventuate.local.db.log.common.OffsetStore;
-import io.eventuate.local.common.status.StatusService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -14,9 +12,6 @@ import java.util.function.Consumer;
 public class MySQLCdcProcessor<EVENT extends BinLogEvent> extends DbLogBasedCdcProcessor<EVENT> {
 
   private DebeziumBinlogOffsetKafkaStore debeziumBinlogOffsetKafkaStore;
-
-  @Autowired(required = false)
-  private StatusService statusService;
 
   public MySQLCdcProcessor(DbLogClient<EVENT> dbLogClient,
                            OffsetStore offsetStore,
@@ -37,26 +32,6 @@ public class MySQLCdcProcessor<EVENT extends BinLogEvent> extends DbLogBasedCdcP
     Optional<BinlogFileOffset> startingBinlogFileOffset = binlogFileOffset;
 
     process(eventConsumer, startingBinlogFileOffset);
-
-    dbLogClient.start(startingBinlogFileOffset, new Consumer<EVENT>() {
-      private boolean couldReadDuplicateEntries = true;
-
-      @Override
-      public void accept(EVENT publishedEvent) {
-        if (couldReadDuplicateEntries) {
-          if (startingBinlogFileOffset.map(s -> s.isSameOrAfter(publishedEvent.getBinlogFileOffset())).orElse(false)) {
-            return;
-          } else {
-            couldReadDuplicateEntries = false;
-          }
-        }
-        eventConsumer.accept(publishedEvent);
-      }
-    });
-
-    if (statusService != null) {
-      statusService.markAsStarted();
-    }
   }
 
   public void stop() {

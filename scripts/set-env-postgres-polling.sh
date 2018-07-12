6b4431a2c8e1937b1f3288b5bc443359e0babd6c
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-07-12 19:27:40 +0300

    #54: Test Hangs from Time to Time. Reorganizing env scripts.

--- scripts/set-env-postgres-polling.sh
+++ scripts/set-env-postgres-polling.sh
@@ -1,20 +1,8 @@
-if [ -z "$DOCKER_HOST_IP" ] ; then
-    if [ -z "$DOCKER_HOST" ] ; then
-      export DOCKER_HOST_IP=`hostname`
-    else
-      echo using ${DOCKER_HOST?}
-      XX=${DOCKER_HOST%\:*}
-      export DOCKER_HOST_IP=${XX#tcp\:\/\/}
-    fi
-fi
+. ./scripts/_set-env.sh
 
-echo DOCKER_HOST_IP is $DOCKER_HOST_IP
-
-export EVENTUATE_EVENT_TRACKER_ITERATIONS=60
 export SPRING_DATASOURCE_URL=jdbc:postgresql://${DOCKER_HOST_IP}/eventuate
 export SPRING_DATASOURCE_USERNAME=eventuate
 export SPRING_DATASOURCE_PASSWORD=eventuate
 export SPRING_DATASOURCE_DRIVER_CLASS_NAME=org.postgresql.Driver
-export EVENTUATELOCAL_KAFKA_BOOTSTRAP_SERVERS=$DOCKER_HOST_IP:9092
-export EVENTUATELOCAL_ZOOKEEPER_CONNECTION_STRING=$DOCKER_HOST_IP:2181
 
+echo SPRING_DATASOURCE_URL=$SPRING_DATASOURCE_URL

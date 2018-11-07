2f17b54eb5963ca60559a6d2c514718720955d6f
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2018-11-07 13:52:56 +0300

    Used mysql 8.0.0. Added trace log.

--- mysql/initialize-database.sql
+++ mysql/initialize-database.sql
@@ -9,13 +9,13 @@ DROP table IF EXISTS  snapshots;
 DROP table IF EXISTS cdc_monitoring;
 
 create table events (
-  event_id varchar(1000) PRIMARY KEY,
-  event_type varchar(1000),
-  event_data varchar(1000) NOT NULL,
-  entity_type VARCHAR(1000) NOT NULL,
-  entity_id VARCHAR(1000) NOT NULL,
-  triggering_event VARCHAR(1000),
-  metadata VARCHAR(1000),
+  event_id varchar(500) PRIMARY KEY,
+  event_type varchar(500),
+  event_data varchar(500) NOT NULL,
+  entity_type VARCHAR(500) NOT NULL,
+  entity_id VARCHAR(500) NOT NULL,
+  triggering_event VARCHAR(500),
+  metadata VARCHAR(500),
   published TINYINT DEFAULT 0
 );
 
@@ -23,21 +23,21 @@ CREATE INDEX events_idx ON events(entity_type, entity_id, event_id);
 CREATE INDEX events_published_idx ON events(published, event_id);
 
 create table entities (
-  entity_type VARCHAR(1000),
-  entity_id VARCHAR(1000),
-  entity_version VARCHAR(1000) NOT NULL,
+  entity_type VARCHAR(500),
+  entity_id VARCHAR(500),
+  entity_version VARCHAR(500) NOT NULL,
   PRIMARY KEY(entity_type, entity_id)
 );
 
 CREATE INDEX entities_idx ON events(entity_type, entity_id);
 
 create table snapshots (
-  entity_type VARCHAR(1000),
-  entity_id VARCHAR(1000),
-  entity_version VARCHAR(1000),
-  snapshot_type VARCHAR(1000) NOT NULL,
-  snapshot_json VARCHAR(1000) NOT NULL,
-  triggering_events VARCHAR(1000),
+  entity_type VARCHAR(500),
+  entity_id VARCHAR(500),
+  entity_version VARCHAR(500),
+  snapshot_type VARCHAR(500) NOT NULL,
+  snapshot_json VARCHAR(500) NOT NULL,
+  triggering_events VARCHAR(500),
   PRIMARY KEY(entity_type, entity_id, entity_version)
 );
 
